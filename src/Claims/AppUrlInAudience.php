429b8ae62589f6a6ef0128ea3bac65b4423ba5a6
Ollie Read <code@ollieread.com>
2025-11-17 20:38:52 +0000

    chore: Add missing claim

---
+++ src/Claims/AppUrlInAudience.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Ollieread\JWT\Claims;
+
+use Illuminate\Container\Attributes\Config;
+use Lcobucci\JWT\Token\RegisteredClaims;
+use Ollieread\JWT\Contracts\JWTClaim;
+
+/**
+ * @implements \Ollieread\JWT\Contracts\JWTClaim<string>
+ */
+final readonly class AppUrlInAudience implements JWTClaim
+{
+    private string $appUrl;
+
+    public function __construct(
+        #[Config('app.url')] string $appUrl
+    )
+    {
+        $this->appUrl = $appUrl;
+    }
+
+    /**
+     * Returns the name of the claim.
+     *
+     * @return lowercase-string
+     */
+    public function name(): string
+    {
+        return RegisteredClaims::AUDIENCE;
+    }
+
+    /**
+     * Returns the value of the claim.
+     *
+     * @return string
+     */
+    public function value(): string
+    {
+        return $this->appUrl;
+    }
+}
